ae465fc288d191ef1ebfd3be6736538f8a524b58
Or Daniel <sunor4@gmail.com>
2023-05-24 23:44:36 +0300

    moved validation to Main, pass parameters to MyWebScrapper after validation

--- lib/src/main/java/mywebscrapper/Main.java
+++ lib/src/main/java/mywebscrapper/Main.java
@@ -1,12 +1,49 @@
 package mywebscrapper;
 
 import mywebscrapper.businesslogic.MyWebScrapper;
+import mywebscrapper.utils.ScrapperUtils;
 
 public class Main {
 
 	public static void main(String[] args) {
-		MyWebScrapper myWebScrapper = new MyWebScrapper();
-		myWebScrapper.run(args);
+		validateInput(args);
+		final String url = args[0];
+		final int maxNumOfUrls = Integer.parseInt(args[1]);
+		final int depth = Integer.parseInt(args[2]);
+		final boolean isUnique = Boolean.parseBoolean(args[3]);
+		MyWebScrapper myWebScrapper = new MyWebScrapper(url, maxNumOfUrls, depth, isUnique);
+		myWebScrapper.run();
 	}
 
+	private static void validateInput(String... args) throws IllegalArgumentException {
+		ScrapperUtils scrapperUtils = new ScrapperUtils();
+		final String url = args[0];
+		final String maxNumOfUrlsStr = args[1];
+		final String depthStr = args[2];
+		final String isUniqueStr = args[3];
+		if (!scrapperUtils.isUrlValid(url)) {
+			throw new IllegalArgumentException(String.format("%s is not a valid url", url));
+		}
+
+		int maxNumOfUrls, depth;
+		try {
+			maxNumOfUrls = Integer.parseInt(maxNumOfUrlsStr);
+			depth = Integer.parseInt(depthStr);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException(e.getMessage());
+		}
+
+		if (maxNumOfUrls <= 0) {
+			throw new IllegalArgumentException("maxNumOfUrls must be greater than 0");
+		}
+
+		if (depth < 0) {
+			throw new IllegalArgumentException("depth must be greater or equal to 0");
+		}
+
+		if (!Boolean.TRUE.toString().equalsIgnoreCase(isUniqueStr)
+				&& !Boolean.FALSE.toString().equalsIgnoreCase(isUniqueStr)) {
+			throw new IllegalArgumentException(String.format("%s is not valid for \"isUnique\" flag", isUniqueStr));
+		}
+	}
 }
--- lib/src/main/java/mywebscrapper/businesslogic/MyWebScrapper.java
+++ lib/src/main/java/mywebscrapper/businesslogic/MyWebScrapper.java
@@ -3,59 +3,27 @@ package mywebscrapper.businesslogic;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
-import mywebscrapper.entity.CrawlerThread;
 import mywebscrapper.utils.ScrapperUtils;
 
 public class MyWebScrapper {
-	Map<String, Boolean> cacheMap = Collections.synchronizedMap(new HashMap<String, Boolean>());
 	private String url = "";
 	private int maxNumOfUrls = 0;
 	private int depth = 0;
 	private boolean isUnique = false;
 
-	public void run(String... args) {
-		validateAndSetInput(args);
-		run(url, maxNumOfUrls, depth, isUnique);
+	public MyWebScrapper(String url, int maxNumOfUrls, int depth, boolean isUnique) {
+		this.url = url;
+		this.maxNumOfUrls = maxNumOfUrls;
+		this.depth = depth;
+		this.isUnique = isUnique;
 	}
 
-	public void run(String url, int maxNumOfUrls, int depth, boolean isUnique) {
-		Thread thread = new Thread(new CrawlerThread(url, maxNumOfUrls, 0, depth, isUnique, cacheMap));
-		thread.start();
-	}
-	
-	private void validateAndSetInput(String... args) throws IllegalArgumentException {
-		ScrapperUtils scrapperUtils = new ScrapperUtils();
-		final String url = args[0];
-		final String maxNumOfUrlsStr = args[1];
-		final String depthStr = args[2];
-		final String isUniqueStr = args[3];
-		if (!scrapperUtils.isUrlValid(url)) {
-			throw new IllegalArgumentException(String.format("%s is not a valid url", url));
-		} else {
-			this.url = url;
-		}
-		
-		try {
-			this.maxNumOfUrls = Integer.parseInt(maxNumOfUrlsStr);
-			this.depth = Integer.parseInt(depthStr);
-		} catch (NumberFormatException e) {
-			throw new IllegalArgumentException(e.getMessage());
-		}
-		
-		if (this.maxNumOfUrls <= 0) {
-			throw new IllegalArgumentException("maxNumOfUrls must be greater than 0");
-		}
-		
-		if (this.depth < 0) {
-			throw new IllegalArgumentException("depth must be greater or equal to 0");
-		}
-		
-		if (Boolean.TRUE.toString().equalsIgnoreCase(isUniqueStr) || Boolean.FALSE.toString().equalsIgnoreCase(isUniqueStr)) {
-			this.isUnique = Boolean.parseBoolean(isUniqueStr);
-		} else {
-			throw new IllegalArgumentException(String.format("%s is not valid for \"isUnique\" flag", isUniqueStr));
-		}
-	}
-	
+	public void run() {
+		ExecutorService executorService = Executors.newFixedThreadPool(maxNumOfUrls);
+		Map<String, Boolean> cacheMap = Collections.synchronizedMap(new HashMap<String, Boolean>());
+    }
 }
